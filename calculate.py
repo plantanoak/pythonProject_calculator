1095e10730420f65a8ba76b9b1e014516f4dbd9a
plantanoak <122216623+plantanoak@users.noreply.github.com>
2023-01-08 21:18:27 +0300

    commit two

--- calculate.py
+++ calculate.py
@@ -1,3 +1,19 @@
 a = input()
 calculate = compile(a, 'string', 'eval')
 print(eval(calculate))
+
+a = float(input())
+b = float(input())
+act = input()
+
+if (act == "/" or act == "mod" or act == "div") and b == 0:
+    c = "Деление на 0!"
+elif act == "+":    c = a + b
+elif act == "-":    c = a - b
+elif act == "/":    c = a / b
+elif act == "*":    c = a * b
+elif act == "mod":  c = a % b
+elif act == "pow":  c = a ** b
+elif act == "div":  c = a // b
+
+print(c)
